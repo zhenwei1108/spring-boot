cdd98d4503fc1418b3f8ac5569676be931ca500d
wegoo <zhenwei1108@163.com>
2024-02-27 22:39:32 +0800

    add init

---
+++ spring-boot-project/spring-boot-tools/spring-boot-gradle-plugin/src/docs/gradle/running/spring-boot-dsl-main-class-name.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+	java
+	application
+	id("org.springframework.boot") version "{gradle-project-version}"
+}
+
+// tag::main-class[]
+springBoot {
+	mainClass.set("com.example.ExampleApplication")
+}
+// end::main-class[]
